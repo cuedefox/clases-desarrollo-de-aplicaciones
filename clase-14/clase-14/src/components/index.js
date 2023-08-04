c2b89ae46e0236bfb8d4e15caf42496afaa6120a
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-08-04 02:02:21 -0300

    add clases 14 y 15

---
+++ clase-14/clase-14/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from './header/index';
+export { default as CategoryItem } from './categories/item/index';
+export { default as Input } from './input/index';
+export { default as CartItem } from './cart/item/index';
+export { default as OrderItem } from './order/item/index';
