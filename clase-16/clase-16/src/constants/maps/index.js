e50df22dbfb9c2ff89dbe2a8ca7ccf97b4fda94c
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-08-09 14:12:41 -0300

    add clase 16

---
+++ clase-16/clase-16/src/constants/maps/index.js
@@ -0,0 +1,6 @@
+export const GOOGLE_API_KEY = 'AIzaSyA0qQBZRnyLOirE8dYv4vxGHwSyA8BsEko';
+export const URL_MAPS = ({ lat, lng, zoom }) =>
+  `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+export const URL_BASE_GEOCODING =
+  'https://maps.googleapis.com/maps/api/geocode/';
